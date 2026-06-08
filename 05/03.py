7e6cbde29c4994a14ee1713a95d2b5a6626caedb
Maksym Motruk <motrukmaksim01@gmail.com>
2026-06-08 16:27:14 +0300

    edit 03.py

--- 05/03.py
+++ 05/03.py
@@ -3,7 +3,7 @@ str_1 = input("Введи будь яке речення: ")
 clean = ""
 
 for char in str_1:
-    if char.isalpha() or char.isspace():
+    if char.isalnum() or char.isspace():
         clean += char
 
 result = "#" + clean.title().replace(' ', '')

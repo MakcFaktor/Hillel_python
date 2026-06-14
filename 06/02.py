a80818027921e06ab951dd767a6273c803a58c02
Maksym Motruk <motrukmaksim01@gmail.com>
2026-06-14 18:47:34 +0300

    add 06/02.py

---
+++ 06/02.py
@@ -0,0 +1,9 @@
+numb = int(input("Введи число від 0 до 8640000: "))
+day = 24 * 60 * 60
+days = numb // 86400
+remainder = numb % 86400
+hours = remainder // 3600
+remainder_2 = remainder % 3600
+minutes = remainder_2 // 60
+remainder_3 = remainder_2 % 60
+print(f"{days} дні, {hours}:{minutes}:{remainder_3}")

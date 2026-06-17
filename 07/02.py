eddd2198f01e6452c268bcfb0ec654ac1ff15852
Maksym Motruk <motrukmaksim01@gmail.com>
2026-06-17 11:15:27 +0300

    add 07/02.py

---
+++ 07/02.py
@@ -0,0 +1,13 @@
+def correct_sentence(text):
+    upper_text = text[0].upper() + text[1:]
+    if upper_text[-1] != ".":
+        upper_text = upper_text + "."
+    return upper_text
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
